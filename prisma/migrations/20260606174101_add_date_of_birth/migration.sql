6593371c2b0d561670ee2faa037baab08e1de978
musa108 <nmanagi23@gmail.com>
2026-06-06 18:42:37 +0100

    feat: add profile fields including date of birth and professional details to Profile model

---
+++ prisma/migrations/20260606174101_add_date_of_birth/migration.sql
@@ -0,0 +1,12 @@
+-- AlterTable
+ALTER TABLE "Profile" ADD COLUMN     "currentOccupation" TEXT,
+ADD COLUMN     "dateOfBirth" TEXT,
+ADD COLUMN     "expertiseInterest" TEXT,
+ADD COLUMN     "familyCompound" TEXT,
+ADD COLUMN     "familyVillage" TEXT,
+ADD COLUMN     "fieldSpecialization" TEXT,
+ADD COLUMN     "highestQualification" TEXT,
+ADD COLUMN     "languagesSpoken" TEXT,
+ADD COLUMN     "localGovernment" TEXT,
+ADD COLUMN     "otherQualifications" TEXT,
+ADD COLUMN     "yearInstalled" TEXT;
